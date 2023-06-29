4edfc4e2aa8c1467f4a28a5e221d4899645d303d
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-29 14:37:31 +0200

    Improved tag styling

--- src/addTags.ts
+++ src/addTags.ts
@@ -3,13 +3,13 @@ import { Entry } from "./createEntry";
 export default function addTags(entry: Entry) {
     const tagsContainer = document.createElement("div");
     entry.container.appendChild(tagsContainer);
-    tagsContainer.className = "flex flex-row";
+    tagsContainer.className = "flex grow flex-row";
 
     for (let i = 0; i < entry.tags.length; i++) {
         const div = document.createElement("div");
         tagsContainer.appendChild(div);
         div.textContent = entry.tags[i];
-        div.className = "px-2 rounded-full";
+        div.className = "px-2 mx-1 rounded-full";
         div.style.backgroundColor = entry.tagColors[i];
     }
 }
