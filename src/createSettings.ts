f35b940af9f403edecc200fedfd24123004d86c5
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-06-29 13:47:49 +0200

    Styled the settings

--- src/createSettings.ts
+++ src/createSettings.ts
@@ -24,7 +24,11 @@ export default function createSettings(entry: Entry): Entry {
 function createSettingsContents(alreadyExistingFolder: string[], alreadyExistingTags: string[]): HTMLDivElement {
     const settingsDiv = document.createElement("div");
     settingsDiv.hidden = true;
-    settingsDiv.className = "py-4 px-6";
+    settingsDiv.className = "border-t-[1px] p-6 pb-5";
+
+    const description = document.createElement("span");
+    settingsDiv.appendChild(description);
+    description.textContent = "Please append this to the accounts name: ";
 
     const folderAndTagsTpAppendToAliasName = document.createElement("span");
     settingsDiv.appendChild(folderAndTagsTpAppendToAliasName);
@@ -43,8 +47,8 @@ function createSettingsContents(alreadyExistingFolder: string[], alreadyExisting
         folderAndTagsTpAppendToAliasName.textContent = folderPart + tagsPart;
     }
 
-    settingsDiv.appendChild(createInputFields("Please enter the folder:", addToFolderPart, settings.folder.separator, alreadyExistingFolder));
-    settingsDiv.appendChild(createInputFields("Please enter the tags:", addToTagsPart, settings.tags.separator, alreadyExistingTags));
+    settingsDiv.appendChild(createInputFields("Folder:", addToFolderPart, settings.folder.separator, alreadyExistingFolder));
+    settingsDiv.appendChild(createInputFields("Tags:", addToTagsPart, settings.tags.separator, alreadyExistingTags));
 
     return settingsDiv;
 
@@ -52,17 +56,21 @@ function createSettingsContents(alreadyExistingFolder: string[], alreadyExisting
 
 function createInputFields(description: string, addCombinedTextTo: (cT: string) => void, combinedTextSeparator: string, alreadyExisting: string[]): HTMLDivElement {
     const container = document.createElement("div");
+    container.className = "flex flex-row pb-1";
 
-        const descriptionElement = document.createElement("span");
+        const descriptionElement = document.createElement("div");
         container.appendChild(descriptionElement);
         descriptionElement.textContent = description;
+        descriptionElement.className = "p-1";
 
         const inputsContainer = document.createElement("div");
         container.appendChild(inputsContainer);
+        inputsContainer.className = "flex flex-row";
 
         const addInputButton = document.createElement("button");
         container.appendChild(addInputButton);
-        addInputButton.textContent = "+";
+        addInputButton.textContent = "➕";
+        addInputButton.className = "pl-2 py-1";
     
     const inputContainerElements: HTMLDivElement[] = [];
     const inputElements: HTMLInputElement[] = [];
@@ -81,15 +89,17 @@ function addInputField(container: HTMLDivElement, containerElements: HTMLDivElem
     const div = document.createElement("div");
     container.appendChild(div);
     containerElements.push(div);
+    div.className = "ml-2 bg-neutral-200 rounded-md p-1";
 
         const removeButton = document.createElement("button");
         div.appendChild(removeButton);
-        removeButton.textContent = "-";
+        removeButton.textContent = "❌";
 
         const input = document.createElement("input");
         div.appendChild(input);
         inputElements.push(input);
         input.value = preValue;
+        input.className = "w-20 ml-2 bg-transparent";
 
     function combineInputs(): string {
         let combinedText = "";
